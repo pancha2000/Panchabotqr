4f537f34efed7a91be1ba3665d043d933f1abe8f
pancha2000 <119848240+pancha2000@users.noreply.github.com>
2024-08-17 21:53:34 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,6 +1,6 @@
 const mega = require("megajs");
 const auth = {
-    email: 'cdilrukshi52@gmail.vom',
+    email: 'cdilrukshi52@gmail.com',
     password: '2006.Shehan',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
